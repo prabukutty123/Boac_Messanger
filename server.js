ef1d3140d6b4d07d1124572635bbee5e2c4619a4
BOACPLTD <praburamm28@gmail.com>
2024-08-27 13:22:24 +0530

    Add

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const SECRET_KEY = 'bxctasRA5j3ZK7E67oljevClO5j8QilVEaf6eGLXMErbRZ3toiCa2QXbFjg4
 mongoose.connect('mongodb://localhost:27017/myfast', {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
-});
+}) .then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 const userSchema = new mongoose.Schema({
   phoneNumber: String,
   name: String,
